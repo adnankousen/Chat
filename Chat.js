7be454cde701c7d8e4d5731235b4bd272d2ffd29
adnankousen <52902007+adnankousen@users.noreply.github.com>
2019-07-16 09:27:53 +0700

    Add files via upload

---
+++ Chat.js
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title>Chat</title>
+</head>
+<body>
+<script id="cid0020000224234445918" data-cfasync="false" async src="//st.chatango.com/js/gz/emb.js" style="width: 250px;height: 350px;">{"handle":"tsukikosubs","arch":"js","styles":{"a":"ffaacc","b":100,"c":"000000","d":"000000","k":"ffaacc","l":"ffaacc","m":"ffaacc","p":"9","q":"ffaacc","r":100,"usricon":1.05,"cnrs":"1","fwtickm":1}}</script>
+</body>
+</html>
